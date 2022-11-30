e58f7042cd3df2f80b065113f0caf1c11573c2a4
zakaria-shahen <zakariay.shahen@gmail.com>
2022-11-30 23:05:27 +0200

    add data Loader

---
+++ src/main/java/com/yeshtery/picturepublishing/DataLoader.java
@@ -0,0 +1,78 @@
+package com.yeshtery.picturepublishing;
+
+import com.yeshtery.picturepublishing.enums.FileStatus;
+import com.yeshtery.picturepublishing.enums.ImageCategory;
+import com.yeshtery.picturepublishing.model.Image;
+import com.yeshtery.picturepublishing.model.User;
+import com.yeshtery.picturepublishing.service.ImageService;
+import com.yeshtery.picturepublishing.service.UserService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    private final UserService userService;
+
+    private final ImageService imageService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public DataLoader(UserService userService, ImageService imageService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.imageService = imageService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        imageService.saveAll(
+                Image.builder()
+                        .category(ImageCategory.MACHINE)
+                        .status(FileStatus.ACCEPT)
+                        .fileName("1.jpg")
+                        .description("any")
+                        .build()
+                ,
+                Image.builder()
+                        .category(ImageCategory.MACHINE)
+                        .status(FileStatus.ACCEPT)
+                        .fileName("2.jpg")
+                        .description("any 2")
+                        .build()
+                ,
+                Image.builder()
+                        .category(ImageCategory.MACHINE)
+                        .status(FileStatus.UNPROCESSED)
+                        .fileName("3.jpg")
+                        .description("any")
+                        .build()
+                ,
+                Image.builder()
+                        .category(ImageCategory.MACHINE)
+                        .status(FileStatus.UNPROCESSED)
+                        .fileName("4.jpg")
+                        .description("any")
+                        .build()
+        );
+
+
+        userService.saveAdmin(
+                User.builder()
+                        .email("admin")
+                        .password(passwordEncoder.encode("admin123"))
+                        .build()
+        );
+
+        userService.saveUser(
+                User.builder()
+                        .email("user")
+                        .password(passwordEncoder.encode("user"))
+                        .build()
+        );
+
+
+    }
+}
